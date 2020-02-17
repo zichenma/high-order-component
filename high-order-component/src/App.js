1ab6469abd01c4f09b78747de473ad9f03d41527
zichenma <mazichen1984@gmail.com>
2020-02-17 07:46:42 +0000

    add unstaged

---
+++ high-order-component/src/App.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import A from './components/A';
+import B from './components/B';
+import C from './components/C';
+
+function App() {
+  return (
+    <Fragment>
+        <A />
+        <B />
+        <C />
+    </Fragment>
+  );
+}
+
+export default App;
