becf995a51683d7c658fa86d25f642bca0772d4a
zichenma <zma@liaisionedu.com>
2020-02-18 23:01:35 -0500

    5_1 finished

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Fragment } from 'react';
 import './App.css';
-import A from './components/A';
-import B from './components/B';
-import C from './components/C';
-import Inheritance_1 from './components/inheritance_1';
-import Inheritance_2 from './components/inheritance_2';
+// import A from './components/A';
+// import B from './components/B';
+// import C from './components/C';
+// import Inheritance_1 from './components/inheritance_1';
+// import Inheritance_2 from './components/inheritance_2';
+import Tabbar from './components/tabbar';
 
 function App() {
   return (
     <Fragment>
-      <A />
+      {/* <A />
       <B name={'Joh Doe'} age={34} />
       <C />
       <Inheritance_1 />
-      <Inheritance_2 />
+      <Inheritance_2 /> */}
+      <Tabbar />
     </Fragment>
   );
 }
