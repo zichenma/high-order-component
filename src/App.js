8b08c9ee46175915bae47b236774532779efe894
zichenma <zma@liaisionedu.com>
2020-02-18 22:21:52 -0500

    4_3 finished

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import A from './components/A';
 import B from './components/B';
 import C from './components/C';
+import Inheritance_1 from './components/inheritance_1';
+import Inheritance_2 from './components/inheritance_2';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <A />
       <B name={'Joh Doe'} age={34} />
       <C />
+      <Inheritance_1 />
+      <Inheritance_2 />
     </Fragment>
   );
 }
