2d487f2bdb14559913ba892c188bff97ab46bdfd
zichenma <zma@liaisionedu.com>
2020-02-18 23:01:14 -0500

    finished 5_1

---
+++ src/components/tabbar/index.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+
+class Tabbar extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div className='tabbar'>123</div>;
+  }
+}
+
+export default Tabbar;
