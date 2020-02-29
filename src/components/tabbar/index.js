9dbcbbb72b2c03081a9b5ebf66cc66aeeac221d5
zichenma <zma@liaisionedu.com>
2020-02-29 11:13:44 -0500

    finished 5_3

--- src/components/tabbar/index.js
+++ src/components/tabbar/index.js
@@ -20,13 +20,25 @@ const tabbarArr = [
   }
 ];
 class Tabbar extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      index: 0
+    }
+  }
+  itemChange = i => {
+    this.setState({
+      ...this.state,
+      index: i
+    })
+  }
   render() {
     return (
       <div className="tabbar">
         <div className='tabbar-content'>
           {
             tabbarArr.map((v, i) => (
-                <div key={i} className='tabbar-item'>
+                <div key={i} className={`tabbar-item ${(this.state.index === i ? 'active' : '')}`} onClick={() => this.itemChange(i)}>
                 <div className={`iconfont ${v.img}`} />
                 <div>{v.text}</div>
                 </div>
